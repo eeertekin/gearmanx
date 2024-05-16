3d183a513cd2cc1939949ab0df9287a49328a1ae
Ertugrul Emre Ertekin <ee@ertek.in>
2024-05-16 18:00:38 +0300

    main: fragmented PoC

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"gearmanx/pkg/admin"
@@ -68,6 +69,9 @@ func Serve(conn net.Conn) {
 		Type: "CLIENT",
 	}
 
+	fragmented_buf := bytes.Buffer{}
+	next_package_at := 0
+
 	for {
 		bsize, err = conn.Read(buf)
 		if err == io.EOF {
@@ -104,8 +108,43 @@ func Serve(conn net.Conn) {
 			continue
 		}
 
-		commands := ParseCommands(buf[0:bsize])
+		commands := []*command.Command{}
 
+		if bytes.HasPrefix(buf, consts.NULLTERM) {
+			size := int(binary.BigEndian.Uint32(buf[8:12]))
+			next_package_at = size + 12
+			if size >= cap(buf) {
+				fmt.Printf("Possible fragmented command %d > %d setting cap to %d \n", size, cap(buf), next_package_at)
+				fragmented_buf.Write(buf)
+			}
+		} else if fragmented_buf.Len() > 0 {
+			if bsize > next_package_at-fragmented_buf.Len() {
+				next_sum := next_package_at - fragmented_buf.Len()
+				fragmented_buf.Write(buf[0:next_sum])
+
+				left_cmd := command.Command{}
+				left_cmd.Parse(buf[next_sum:bsize])
+				commands = append(commands, &left_cmd)
+			} else {
+				fragmented_buf.Write(buf[0:bsize])
+			}
+		}
+
+		if fragmented_buf.Len() > 0 && next_package_at == fragmented_buf.Len() {
+			fmt.Printf("Fragment size : %d\n", fragmented_buf.Len())
+			next_package_at = -1
+			cmd := command.Command{}
+			cmd.Parse(fragmented_buf.Bytes())
+			fragmented_buf.Reset()
+			commands = append([]*command.Command{&cmd}, commands...)
+
+		} else if fragmented_buf.Len() == 0 {
+			commands = ParseCommands(buf[0:bsize])
+
+		}
+
+		// To disable parse packages, open it
+		// commands := ParseCommands(buf[0:bsize])
 		for i := range commands {
 			HandleCommand(conn, &iam, commands[i])
 		}
