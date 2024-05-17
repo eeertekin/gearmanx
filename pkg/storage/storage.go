284afb3bd0ced2a5049e86f0996cbb9d8f033fbe
Ertugrul Emre Ertekin <ee@ertek.in>
2024-05-17 17:04:55 +0300

    storage: NewStorage()

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -6,6 +6,27 @@ var Backend *Redis
 
 func AddJob(job *models.Job) {
 	Backend.AddJob(job)
+func NewStorage(uri string) error {
+	u, err := url.Parse(uri)
+	if err != nil {
+		fmt.Printf("Storage is not available, check URI %s\n", uri)
+		os.Exit(1)
+	}
+
+	if u.Scheme == "redis" {
+		if backend, err = NewRedisBackend(u.Host); err != nil {
+			return err
+		}
+		return nil
+	} else if u.Scheme == "mem" {
+		if backend, err = NewMemBackend(u.Host); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	return fmt.Errorf("Storage is not available, check URI %s\n", uri)
+}
 }
 
 func DeleteJob(ID []byte) {
